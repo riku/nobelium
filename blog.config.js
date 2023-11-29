7f73aff8e946c247b4e5b0a37106e5d0a288cca6
Riku <lu.riku@gmail.com>
2023-11-29 19:11:20 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -33,7 +33,7 @@ const BLOG = {
       domainId: '' // e.g '0e2257a8-54d4-4847-91a1-0311ea48cc7b'
     },
     gaConfig: {
-      measurementId: 'G-8BXFGZRCEM' // e.g: G-XXXXXXXXXX
+      measurementId: 'G-JWMLXJT9DW' // e.g: G-XXXXXXXXXX
     }
   },
   comment: {
