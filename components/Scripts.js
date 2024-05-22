00e7470fd40fe45c2c0f68e30afb05e81f5ed66b
Riku <lu.riku@gmail.com>
2024-05-22 10:07:56 +0800

    Revert "Update Scripts.js"
    This reverts commit 387c56abbf24b7a1e23e43a26f0fddc67c37a0a5.

--- components/Scripts.js
+++ components/Scripts.js
@@ -28,23 +28,6 @@ const Scripts = () => {
           </Script>
         </>
       )}
-      {/* Matomo */}
-      <Script id="matomo" strategy="lazyOnload">
-        {`
-          var _paq = window._paq = window._paq || [];
-          /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
-          _paq.push(['trackPageView']);
-          _paq.push(['enableLinkTracking']);
-          (function() {
-            var u="//218.78.53.236:8000/matomo/";
-            _paq.push(['setTrackerUrl', u+'matomo.php']);
-            _paq.push(['setSiteId', '2']);
-            var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
-            g.async=true; g.src=u+'matomo.js'; s.parentNode.insertBefore(g,s);
-          })();
-        `}
-      </Script>
-      {/* End Matomo Code */}
     </>
   )
 }
