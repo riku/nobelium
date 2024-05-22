7e30df2cf16a38bdc01ceb881b7be0288e7d5972
Riku <lu.riku@gmail.com>
2024-05-22 09:09:08 +0800

    Update Scripts.js

--- components/Scripts.js
+++ components/Scripts.js
@@ -15,17 +15,26 @@ const Scripts = () => {
       )}
       {BLOG.analytics && BLOG.analytics.provider === 'ga' && (
         <>
-          <Script
-            src={`https://www.googletagmanager.com/gtag/js?id=${BLOG.analytics.gaConfig.measurementId}`}
-          />
-          <Script strategy="lazyOnload" id="ga">
-            {`window.dataLayer = window.dataLayer || [];
-              function gtag(){dataLayer.push(arguments);}
-              gtag('js', new Date());
-              gtag('config', '${BLOG.analytics.gaConfig.measurementId}', {
-                page_path: window.location.pathname,
-              });`}
-          </Script>
+
+        <!-- Matomo -->
+        <script>
+          var _paq = window._paq = window._paq || [];
+          /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
+          _paq.push(["setDocumentTitle", document.domain + "/" + document.title]);
+          _paq.push(["setCookieDomain", "*.kuapp.com"]);
+          _paq.push(['trackPageView']);
+          _paq.push(['enableLinkTracking']);
+          (function() {
+            var u="//218.78.53.236:8000/matomo/";
+            _paq.push(['setTrackerUrl', u+'matomo.php']);
+            _paq.push(['setSiteId', '2']);
+            var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+            g.async=true; g.src=u+'matomo.js'; s.parentNode.insertBefore(g,s);
+          })();
+        </script>
+        <!-- End Matomo Code -->
+
+        
         </>
       )}
     </>
